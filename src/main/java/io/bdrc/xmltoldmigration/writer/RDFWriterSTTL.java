335ceb14566495055ff42c5fb04b288830557e78
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-08-02 17:02:21 +0200

    initial setup of an STTL writer

---
+++ src/main/java/io/bdrc/xmltoldmigration/writer/RDFWriterSTTL.java
@@ -0,0 +1,11 @@
+package io.bdrc.xmltoldmigration.writer;
+
+import org.apache.jena.riot.adapters.RDFWriterRIOT;
+
+public class RDFWriterSTTL extends RDFWriterRIOT {
+
+    public RDFWriterSTTL(String jenaName) {
+        super(jenaName);
+    }
+
+}
---
+++ src/main/java/io/bdrc/xmltoldmigration/writer/STTLWriter.java
@@ -0,0 +1,46 @@
+package io.bdrc.xmltoldmigration.writer;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.apache.jena.atlas.io.IndentedWriter;
+import org.apache.jena.graph.Graph;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.LangBuilder;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.riot.RDFWriterRegistry;
+import org.apache.jena.riot.system.PrefixMap;
+import org.apache.jena.riot.system.RiotLib;
+import org.apache.jena.riot.system.StreamOps;
+import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.riot.writer.TurtleWriterBase;
+import org.apache.jena.riot.writer.WriterGraphRIOTBase;
+import org.apache.jena.riot.writer.WriterStreamRDFBlocks;
+import org.apache.jena.sparql.util.Context;
+
+public class STTLWriter extends TurtleWriterBase {
+
+    public static Lang lang = null;
+    
+    public static void registerWriter() {
+        if (lang != null) return;
+        lang = LangBuilder.create("STTL", "stupid-content").build();
+        RDFLanguages.register(lang);
+        RDFFormat format = new RDFFormat(lang);
+        RDFWriterRegistry.register(lang, format);
+        RDFWriterRegistry.register(format, new STTLWriterFactory()) ;
+    }
+    
+    @Override
+    public Lang getLang() {
+        return lang;
+    }
+
+    @Override
+    protected void output(IndentedWriter iOut, Graph graph, PrefixMap prefixMap, String baseURI, Context context) {
+        SortedTurtleShell w = new SortedTurtleShell(iOut, prefixMap, baseURI, context) ;
+        w.write(graph) ;
+    }
+
+}
