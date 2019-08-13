3479fdf7f091d4adb09de12eaac822f9a90d66f5
Chris Tomlinson <ct@moonvine.org>
2019-08-13 15:09:24 -0500

    added test for getLiteral - validates Khmer handling at that point

--- src/test/java/io/bdrc/xmltoldmigration/MigrationTest.java
+++ src/test/java/io/bdrc/xmltoldmigration/MigrationTest.java
@@ -1,11 +1,5 @@
 package io.bdrc.xmltoldmigration;
 
-import static io.bdrc.libraries.LangStrings.addEwtsShad;
-import static io.bdrc.libraries.LangStrings.isAllTibetanUnicode;
-import static io.bdrc.libraries.LangStrings.isLikelyEnglish;
-import static io.bdrc.libraries.LangStrings.isMostLikelyEwts;
-import static io.bdrc.libraries.LangStrings.isStandardTibetan;
-import static io.bdrc.libraries.LangStrings.normalizeTibetan;
 import static io.bdrc.libraries.Models.BDA;
 import static io.bdrc.libraries.Models.setPrefixes;
 import static org.junit.Assert.assertEquals;
@@ -18,21 +12,24 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.validation.Validator;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -105,41 +102,25 @@ public class MigrationTest
 	}
 	
 	@Test
-	public void testEwtsShad() {
-	    assertTrue("bla ma/".equals(addEwtsShad("bla ma")));
-	    assertTrue("ngo /".equals(addEwtsShad("ngo")));
-	    assertTrue("nga /".equals(addEwtsShad("nga")));
-	    assertTrue("ngag".equals(addEwtsShad("ngag")));
-	    assertTrue("ga".equals(addEwtsShad("ga")));
-	    assertTrue("gi".equals(addEwtsShad("gi")));
-	    assertTrue("she".equals(addEwtsShad("she")));
-	    assertTrue("tshe/".equals(addEwtsShad("tshe")));
-	    assertTrue("ko".equals(addEwtsShad("ko")));
-	}
-	
-	@Test
-	public void textEwts() {
-	    List<String> conversionWarnings = new ArrayList<String>();
-	    String res = toUnicode("pa'ng", conversionWarnings);
-	    assertTrue(res.equals("པའང"));
-	    assertTrue(conversionWarnings.size()==0);
-	    conversionWarnings = new ArrayList<String>();
-	    res = toUnicode("be'u'i'o", conversionWarnings);
-        assertTrue(res.equals("བེའུའིའོ"));
-        assertTrue(conversionWarnings.size()==0);
-        conversionWarnings = new ArrayList<String>();
-        res = toUnicode("pa'm", conversionWarnings);
-        assertTrue(res.equals("པའམ"));
-        assertTrue(conversionWarnings.size()==0);
-        assertTrue(normalizeTibetan("དྷ་དཹ་").equals("དྷ་དླཱྀ་"));
-        assertTrue(normalizeTibetan("\u0F81").equals("\u0F71\u0F80"));
-        assertTrue(normalizeTibetan("\u0F76").equals("\u0FB2\u0F80"));
-        assertTrue(isMostLikelyEwts("myang stod khu le'i rgya rigs"));
-        assertFalse(isMostLikelyEwts("my tailor is rich"));
-        assertFalse(isMostLikelyEwts("associated w / 11th cent. master, P4CZ15480 ?"));
-        assertTrue(isAllTibetanUnicode("སྡེ་དགེ་《བཀའ་འགྱུར་》ནི་ཁམས་སྡེ་དགེ་ཆོས་རྒྱལ་བསྟན་པ་ཚེ་རིང་གིས་སྦྱིན་བདག་མཛད་དེ་བོད་རབ་བྱུང་བཅུ་གཉིས་པའི་ས་བྱ་ ༼སྤྱི་ལོ་ ༡༧༢༩༽ ནས་ཆུ་གླང་ ༼སྤྱི་ལོ ་༡༧༣༣༽ བར་བགྲང་བྱ་ལྔའི་ངོ་མཐོང་བར་བརྩོན་པ་ཆེན་པོས་པར་དུ་བཞེངས་ཞིང་། སྡེ་དགེ་དགོན་ཆེན་ཏེ་ལྷུན་གྲུབ་སྟེང་གི་པར་ཁང་ཆོས་མཛོད་ཆེན་མོར་བཞུགས་པ་ལ་སྡེ་དགེའི་མཚལ་པར་ཞེས་ཡོངས་སུ་གྲགས༎ 《བཀའ་འགྱུར་》འདིའི་ཡི་གེའི་ཕྱི་མོ་ཇི་ལྟར་བསྡུས་པའི་ཚུལ་ནི། བོད་ཀྱི་ལོ་པཎ་མང་པོས་བརྟགས་ཤིང་དཔྱད་ནས། ཞུས་དག་མཛད་ཅིང་མཆན་བུ་སོགས་བཏབ་པ་གངས་རིའི་ལྗོངས་སུ་ཁུངས་ཐུབ་ཀྱི་ཕྱི་མོར་གྱུར་ཏེ། འཕྱིང་བ་སྟག་རྩེའི་ཕོ་བྲང་དུ་བཞུགས་པའི་ཚལ་པ་བཀའ་འགྱུར་ཕྱིས་འཇང་ཡུལ་དུ་སྤྱན་དྲངས་ནས་པར་དུ་བསྒྲུབས་པའི་འཇང་ངམ་ལི་ཐང་《བཀའ་འགྱུར་》ནི་པར་གཞི་འདིའི་མ་ཕྱི་གཙོ་བོར་བཟུང་བ་དང་དེའི་སྟེང་དུ་སྒ་ཨ་གཉན་པཀྵིའི་ཐུགས་དམ་གྱི་རྟེན་དུ་གྱུར་པ་《བཀའ་འགྱུར་》ཤིན་ཏུ་དག་པ་དང་། གཞན་ཡང་དཔེ་རྒྱུན་རྙིང་མ་ཁུངས་བཙུན་འགའ་ཞིག་ལ་གོ་སྡུར་མཛད་ཅིང་། མདོ་སྟོད་ལྷོ་རྫོང་གི་ཕོ་བྲང་དུ་བཞུགས་པའི་《བཀའ་འགྱུར་》ཡང་སྤྱན་དྲངས་ཏེ། དེ་དང་ཡང་སྒོ་བསྟུན་ནས་པར་དུ་བསྒྲུབས་པར་བཤད་དོ༎  དེ་ཡང་《བཀའ་འགྱུར་》འདིའི་ཡི་གེ་མཁན་དང་ཞུ་དག་པ་རྣམས་ཀྱི་སློབ་སྟོན་པའམ་ཞུས་ཆེན་པ་ནི། སི་ཏུ་ཆོས་ཀྱི་འབྱུང་གནས་སམ་ཀརྨ་བསྟན་པའི་ཉིན་བྱེད་གཙུག་ལག་ཆོས་ཀྱི་སྣང་བ་ཞེས་ཞུ་བ་ཡིན་ཏེ། ཁོང་གི་ཞུས་ཆེན་མཛད་ནས་དཀར་ཆག་《བདེ་བར་གཤེགས་པའི་བཀའ་གངས་ཅན་གྱི་བརྡས་དྲངས་པའི་ཕྱི་མོའི་ཚོགས་ཇི་སྙེད་པ་པར་དུ་བསྒྲུབས་པའི་ཚུལ་ལས་ཉེ་བར་བརྩམས་པའི་གཏམ་བཟང་པོ་བློ་ལྡན་མོས་པའི་ཀུནྡ་ཡོངས་སུ་ཁ་བྱེ་བའི་ཟླ་འོད་གཞོན་ནུའི་འཁྲི་ཤིང་》ཞེས་པ་མཛད་ཅིང་།དེ་ནི་《བཀའ་འགྱུར་》དཀར་ཆག་རྣམས་ལས་ཆེས་རྒྱས་པ་ཞིག་གོ༎"));
-        assertTrue(isAllTibetanUnicode("སྡེ་དགེ་བཀའ་འགྱུར། མདོ་སྟོད་སྡེ་དགེ་ཆོས་རྒྱལ་བསྟན་པ་ཚེ་རིང་ ༼༡༦༧༨-༡༧༣༨༽ ནས་སྦྱིན་བདག་མཛད་ནས། ལི་ཐང་མཚལ་དཔར། སྒ་ཨ་གཉེན་པཀྵིའི་ཐུགས་དམ་རྟེན་གྱི་བཀའ་འགྱུར་ཤིན་ཏུ་དག་པ། མདོ་སྟོད་ལྷོ་རྫོང་གི་ཕོ་བྲང་དུ་བཞུགས་པའི་ཐང་པོ་ཆེའི་བཀའ་འགྱུར་སོགས་དཔེ་རྒྱུན་རྙིང་མ་ཁུངས་བཙུན་འགའ་ཞིག་གོ་བསྡུར་ནས། ༡༧༢༩ ལོར་དཔར་གཞི་གསར་བཞེངས་དབུ་བརྩམས་ནས་བགྲང་བྱ་ལྔའི་ངོ་མཐོང་ ༡༧༣༣ བར་བརྩོན་པ་ཆེན་པོས་དཀར་ཆག་ཐེ་པའི་པོད་བརྒྱ་དང་གསུམ་ལེགས་པར་བསྒྲུབས་ནས་སྡེ་དགེ་དགོན་ཆེན་ལྷུན་གྲུབ་སྟེང་གི་དཔར་ཁང་ཆོས་མཛོད་ཆེན་མོར་བཞུགས་པ་ལ་སྡེ་དགེ་མཚལ་དཔར་ཞེས་གྲགས་ཅན་དེ་བྱུང་།།"));
-        assertTrue(isLikelyEnglish("Biography of the famous Tibetan physician and scholar, Troru Tsenam."));
+	public void testGetLiteral() {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.newDocument();
+            Element elem = doc.createElement("title");
+            elem.setTextContent("maṅgalatthadīpanī aṭṭhakathāmaṅgalasūtra");
+            elem.setAttribute("lang", "pāli");
+            elem.setAttribute("encoding", "kmfemc");
+            Model model = ModelFactory.createDefaultModel();
+            String dflt = "ru";
+            String propHint = "workTitle";
+            String RID = "W1FEMC010006";
+            String subRID = "W1FEMC010006_001";
+            Literal lit = CommonMigration.getLiteral(elem, dflt, model, propHint, RID, subRID);
+            assertTrue("pi-x-kmfemc".equals(lit.getLanguage()));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 	}
 	
 	@Test
@@ -152,17 +133,6 @@ public class MigrationTest
 	    assertTrue(CommonMigration.getRIDFromTbrcUrl("http://mercury.tbrc.org/link?RID=O3LS12537|O3LS125373LS13489$W8039").equals("O3LS125373LS13489"));
 	    assertTrue(CommonMigration.getRIDFromTbrcUrl("http://tbrc.org/?locale=bo#library_work_Object-W1PD107999").equals("W1PD107999"));
 	    assertTrue(CommonMigration.getRIDFromTbrcUrl("http://tbrc.org/link/?RID=T1CZ28#library_topic_Object-T1CZ28").equals("T1CZ28"));
-	}
-	
-	@Test
-	public void testHunspell() {
-	    assertTrue(isStandardTibetan("བོད"));
-	    assertTrue(isStandardTibetan("བོད་བོད་ བོད་"));
-	    assertFalse(isStandardTibetan("བབབོ་ད་དདཨོ་"));
-	    assertFalse(isStandardTibetan("བབབོ་ད་དདཨོ་"));
-	    assertFalse(isStandardTibetan("བོད a"));
-	    assertFalse(isStandardTibetan("abc"));
-	    assertFalse(isStandardTibetan("རཀག"));
 	}
 	
 	@Test
